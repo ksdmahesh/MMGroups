86c89f49e705349d41d7c0b65ef364cfcdb5a198
Mahesh <ksdmahesh@hotmail.com>
2020-03-22 15:32:11 +0530

    reordered

---
+++ Application/Web/MVC/LanguageLearner/LanguageLearner/App_Start/BundleConfig.cs
@@ -0,0 +1,38 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace LanguageLearner
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/open-iconic-bootstrap.css",
+                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css/fonts").Include(
+                      "~/fonts/open-iconic.eot",
+                      "~/fonts/open-iconic.otf",
+                      "~/fonts/open-iconic.svg",
+                      "~/fonts/open-iconic.ttf",
+                      "~/fonts/open-iconic.woff"));
+            
+        }
+    }
+}
