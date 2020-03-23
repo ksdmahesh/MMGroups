cf9f4c9c7ef153aeec7525972e8e9d27e0626fe5
Mahesh <ksdmahesh@hotmail.com>
2020-03-23 12:47:35 +0530

    footer

--- Application/Web/React/LanguageLearner/src/view/shared/layout/footer.tsx
+++ Application/Web/React/LanguageLearner/src/view/shared/layout/footer.tsx
@@ -1,13 +1,22 @@
 import * as React from 'react';
 import BaseComponent from '../helper/baseComponent';
+import { Toolbar, Typography, AppBar } from '@material-ui/core';
 
 class Footer extends BaseComponent {
 
     render() {
         return (
             <footer id="footer">
-                <div>
-                    &nbsp;{`Language Learner `}&copy;
+                <div style={{ flexGrow: 1 }}>
+                    <AppBar position="static">
+                        <Toolbar
+                            style={{ minHeight: '48px' }}
+                        >
+                            <Typography variant={'subtitle1'} style={{ flexGrow: 1, textAlign: 'center' }}>
+                                &nbsp;{`Language Learner `}&copy;{new Date().getFullYear()}
+                            </Typography>
+                        </Toolbar>
+                    </AppBar>
                 </div>
             </footer>
         );
