a0fa538922128712cb628b4966f8a3326e4705d2
Mahesh <ksdmahesh@hotmail.com>
2021-03-03 16:19:59 +0530

    okok

--- Application/Web/React/converters/src/convert/CsstoJs.ts
+++ Application/Web/React/converters/src/convert/CsstoJs.ts
@@ -53,6 +53,17 @@ type CssRule = {
 	value?: string
 }
 
+type JsRule = {
+	// default, media, keyframes
+	[x: string]: {
+		// selector
+		[x: string]: {
+			// React.CSSProperties
+			[x: string]: string | number
+		}
+	}
+}
+
 type Item = {
 	originalValue: string,
 	selectors: Array<string>,
@@ -116,45 +127,65 @@ type ConvertionProps = {
 
 //#endregion
 
-//#region Private Functions
+const testRule: CssRule[] = [];
+let isSelector = false;
+let isMedia = false;
+let isProperty = false;
+let rule: CssRule = {};
+const path: { type: CssTypes, value: string }[] = [];
+let jsRule: JsRule = {};
+let convertionAttributes: ConvertionProps | undefined;
+const pattern = /-|\.|#|=|\]|\[|\)|\(|:/igm;
 
-const clone = (data: object) => JSON.parse(JSON.stringify(data));
-
-const toProperty = (name: string) => {
-	if (name.charAt(0) === "-") name = name.slice(0);
-
-	return name.replace(/[^a-z0-9]([a-z0-9])?/gi, (_, l) => l ? l.toUpperCase() : '');
-}
-
-const toSelectors = (name: string) => {
-	return name.split(",").map(nameMap);
-}
-
-const nameMap = (currentName: string) => {
-	currentName = currentName.trim();
-	let newName = "";
-
-	if (currentName.charAt(0) === ".") {
-		newName += "Class";
-		currentName = currentName.slice(1);
-	} else if (currentName.charAt(0) === "#") {
-		newName += "Id";
-		currentName = currentName.slice(1);
-	} else {
-		newName += "Element";
+const getPath = (value: string) => {
+	switch (rule.type) {
+		case CssTypes.selector:
+			switch (convertionAttributes?.caseType) {
+				case 'pascal':
+					return value.split(',')[0].split(pattern).map(result => result.replace(/^\w/, c => c.toUpperCase())).join('');
+				case 'camel':
+					return value.split(',')[0].split(pattern).filter(a => a).map((result, index) => result.replace(/^\w/, c => index ? c.toUpperCase() : c)).join('');
+				default:
+					break;
+			}
+			return value.split(',')[0].split(pattern).join('');
+		case CssTypes.media:
+			return value;
+		case CssTypes.property:
+			return value.split(',')[0].split(pattern).filter(a => a).map((result, index) => result.replace(/^\w/, c => index ? c.toUpperCase() : c)).join('');
+		default:
+			break;
 	}
 
-	return `${currentName.replace(/([^a-z0-9])([a-z0-9])?/gi, (_, c, l) => (l ? ((c === "," || c === " ") ? l.toLowerCase() : l.toUpperCase()) : ''))}${newName}`;
+	return value;
 }
 
-const tokenizer = (code: string[]) => {
-	const rules: CssRule[] = [];
-	let rule: CssRule = clone(rules[0] || {});
-	let prevRule: CssRule = {};
-	let isSelectorOpened = false;
-	let isSelectorClosed = false;
-	let isPropertyClosed = false;
+const assignRule = () => {
+	let currentRule: any = jsRule;
+	let prevPath: { type: CssTypes, value: string } | undefined = undefined;
+	path.forEach((activePath, index) => {
+		try {
+			if (activePath.type === CssTypes.value) {
+				if (prevPath) {
+					currentRule[prevPath.value] = `${activePath.value || ''}`;
+				}
+			} else {
+				if (!currentRule[activePath.value]) {
+					currentRule[activePath.value] = {};
+				}
+				
+				prevPath = activePath;
+				currentRule = currentRule[activePath.value];
+			}
+		} catch (er) {
 
+		}
+	});
+
+	testRule.push(rule);
+}
+
+const convertToJS = (code: string[]) => {
 	for (const char of code) {
 		if (rule.type) {
 			switch (char) {
@@ -163,8 +194,11 @@ const tokenizer = (code: string[]) => {
 						rule.actualValue += char;
 						rule.value += char;
 					} else {
-						rules.push(rule);
-						prevRule = clone(rule);
+						path.push({ type: rule.type, value: `${rule.actualValue}` });
+						assignRule();
+						path.pop();
+						path.pop();
+						isProperty = false;
 						rule = {};
 					}
 					continue;
@@ -173,10 +207,10 @@ const tokenizer = (code: string[]) => {
 						rule.actualValue += char;
 						rule.value += char;
 					} else {
-						rules.push(rule);
-						prevRule = clone(rule);
+						path.push({ type: rule.type, value: getPath(`${rule.actualValue}`) });
+						assignRule();
+						isProperty = rule.type === CssTypes.property;
 						rule = {};
-						isPropertyClosed = true;
 					}
 					continue;
 				case SpecialChars.SelectorOrMediaClose:
@@ -186,15 +220,36 @@ const tokenizer = (code: string[]) => {
 						const startLength = rule.actualValue?.split('{').length;
 						const endLength = rule.actualValue?.split('}').length;
 						if (startLength === endLength) {
-							rules.push(rule);
-							prevRule = clone(rule);
+							assignRule();
+							if (isSelector) {
+								isSelector = false;
+								path.pop();
+							}
+							if (isProperty) {
+								isProperty = false;
+								path.pop();
+							}
+							if (rule.type === CssTypes.media) {
+								isMedia = false;
+								path.pop();
+							}
 							rule = {};
 						}
 					} else {
-						rules.push(rule);
-						prevRule = clone(rule);
+						assignRule();
+						if (isSelector) {
+							isSelector = false;
+							path.pop();
+						}
+						if (isProperty) {
+							isProperty = false;
+							path.pop();
+						}
+						if (rule.type === CssTypes.media) {
+							isMedia = false;
+							path.pop();
+						}
 						rule = {};
-						isSelectorClosed = true;
 					}
 					continue;
 				case SpecialChars.SelectorOrMediaOpen:
@@ -206,20 +261,20 @@ const tokenizer = (code: string[]) => {
 							rule.actualValue += char;
 							rule.value += char;
 						} else {
-							rules.push(rule);
-							prevRule = clone(rule);
+							path.push({ type: rule.type, value: getPath(`${rule.actualValue}`) });
+							assignRule();
+							isSelector = rule.type === CssTypes.selector;
+							isMedia = rule.type === CssTypes.media;
+							isProperty = false;
 							rule = {};
-							isSelectorOpened = rule.type === CssTypes.selector;
 						}
 					}
 					continue;
 				case SpecialChars.SingleComment1:
-				case SpecialChars.SingleComment1:
+				case SpecialChars.SingleComment2:
 					rule.actualValue += char;
 					rule.value += char;
 					if (rule.type === CssTypes.comment && rule.subType === CssSubTypes.singleLineComment) {
-						rules.push(rule);
-						prevRule = clone(rule);
 						rule = {};
 					}
 					continue;
@@ -230,8 +285,6 @@ const tokenizer = (code: string[]) => {
 						const startLength = rule.actualValue?.split('/*').length;
 						const endLength = rule.actualValue?.split('*/').length;
 						if (startLength === endLength) {
-							rules.push(rule);
-							prevRule = clone(rule);
 							rule = {};
 						}
 					}
@@ -239,11 +292,17 @@ const tokenizer = (code: string[]) => {
 				default:
 					break;
 			}
-		} else {
-			if (char === SpecialChars.SelectorOrMediaClose) {
-				isSelectorClosed = true;
-				continue;
+		} else if (char === SpecialChars.SelectorOrMediaClose) {
+			if (isMedia) {
+				path.pop();
+				isMedia = false;
+			} else {
+				path.pop();
+				isSelector = false;
+				isProperty = false;
 			}
+
+			continue;
 		}
 
 		switch (rule.type) {
@@ -282,7 +341,7 @@ const tokenizer = (code: string[]) => {
 				rule.actualValue += char;
 				break;
 			default:
-				if (prevRule.type !== CssTypes.property) {
+				if (!isProperty && !isSelector) {
 					switch (char) {
 						case Chars.Class:
 							rule = {
@@ -290,7 +349,7 @@ const tokenizer = (code: string[]) => {
 								subType: CssSubTypes.class,
 								value: '',
 								actualValue: `${char}`
-							}
+							};
 							break;
 						case Chars.Id:
 							rule = {
@@ -298,7 +357,7 @@ const tokenizer = (code: string[]) => {
 								subType: CssSubTypes.id,
 								value: '',
 								actualValue: `${char}`
-							}
+							};
 							break;
 						case Chars.Comment:
 							rule = {
@@ -306,7 +365,7 @@ const tokenizer = (code: string[]) => {
 								subType: undefined,
 								value: ``,
 								actualValue: `${char}`
-							}
+							};
 							break;
 						case Chars.Media:
 							rule = {
@@ -314,53 +373,294 @@ const tokenizer = (code: string[]) => {
 								subType: undefined,
 								value: '',
 								actualValue: `${char}`
-							}
+							};
 							break;
 						default:
-							// '\s,-,[,\\,|,*,:'
-							if (char !== ' ' && char != SpecialChars.SingleComment1 && char != SpecialChars.SingleComment2) {
-								if (prevRule.type === CssTypes.selector || isSelectorOpened) {
-									isSelectorOpened = false;
-									rule = {
-										type: CssTypes.property,
-										subType: undefined,
-										value: `${char}`,
-										actualValue: `${char}`
-									}
-								} else {
-									rule = {
-										type: CssTypes.selector,
-										subType: CssSubTypes.element,
-										value: `${char}`,
-										actualValue: `${char}`
-									}
-								}
+							if (char !== ' ' && char !== SpecialChars.SingleComment1 && char !== SpecialChars.SingleComment2) {
+								rule = {
+									type: CssTypes.selector,
+									subType: CssSubTypes.element,
+									value: `${char}`,
+									actualValue: `${char}`
+								};
 							}
 							break;
 					}
-				} else {
-					if (prevRule.type === CssTypes.property && isPropertyClosed) {
-						isPropertyClosed = false;
+					break;
+				} else if (char !== ' ' && char !== SpecialChars.SingleComment1 && char !== SpecialChars.SingleComment2) {
+					if (isProperty) {
 						rule = {
 							type: CssTypes.value,
 							subType: undefined,
 							value: `${char}`,
 							actualValue: `${char}`
-						}
+						};
+					} else if (isSelector) {
+						rule = {
+							type: CssTypes.property,
+							subType: undefined,
+							value: `${char}`,
+							actualValue: `${char}`
+						};
 					}
 				}
-				break;
 		}
 	}
 
-	console.log(rules);
-
-	return [''];// iteratorProps.tokens.map(token => token.value.trim()).filter(token => token);
+	console.log(jsRule)
 }
+
+//#region Private Functions
+
+export const clone = (data: object) => JSON.parse(JSON.stringify(data));
+
+const toProperty = (name: string) => {
+	if (name.charAt(0) === "-") name = name.slice(0);
+
+	return name.replace(/[^a-z0-9]([a-z0-9])?/gi, (_, l) => l ? l.toUpperCase() : '');
+}
+
+const toSelectors = (name: string) => {
+	return name.split(",").map(nameMap);
+}
+
+const nameMap = (currentName: string) => {
+	currentName = currentName.trim();
+	let newName = "";
+
+	if (currentName.charAt(0) === ".") {
+		newName += "Class";
+		currentName = currentName.slice(1);
+	} else if (currentName.charAt(0) === "#") {
+		newName += "Id";
+		currentName = currentName.slice(1);
+	} else {
+		newName += "Element";
+	}
+
+	return `${currentName.replace(/([^a-z0-9])([a-z0-9])?/gi, (_, c, l) => (l ? ((c === "," || c === " ") ? l.toLowerCase() : l.toUpperCase()) : ''))}${newName}`;
+}
+
+// const tokenizer = (code: string[]) => {
+// 	const rule: CssRule = {};
+// 	const jsRule: JsRule = {};
+// 	let path: string[] = [];
+
+// 	for (const char of code) {
+// 		if (rule.type) {
+// 			switch (char) {
+// 				case SpecialChars.Value:
+// 					if (rule.type !== CssTypes.value) {
+// 						rule.actualValue += char;
+// 						rule.value += char;
+// 					} else {
+// 						let currentRule: any = jsRule;
+// 						path.forEach(a => {
+// 							if (!currentRule[a]) {
+// 								currentRule[a] = {};
+// 							}
+// 							currentRule = currentRule[a];
+// 						})
+// 						jsRule[path.join('.')] = `${rule.value || ''}`;
+// 					}
+// 					continue;
+// 				case SpecialChars.Property:
+// 					if (rule.type !== CssTypes.property) {
+// 						rule.actualValue += char;
+// 						rule.value += char;
+// 					} else {
+// 						rules.push(rule);
+// 						prevRule = clone(rule);
+// 						rule = {};
+// 						isPropertyClosed = true;
+// 					}
+// 					continue;
+// 				case SpecialChars.SelectorOrMediaClose:
+// 					if (rule.subType === CssSubTypes.keyFrames) {
+// 						rule.actualValue += char;
+// 						rule.value += char;
+// 						const startLength = rule.actualValue?.split('{').length;
+// 						const endLength = rule.actualValue?.split('}').length;
+// 						if (startLength === endLength) {
+// 							rules.push(rule);
+// 							prevRule = clone(rule);
+// 							rule = {};
+// 						}
+// 					} else {
+// 						rules.push(rule);
+// 						prevRule = clone(rule);
+// 						rule = {};
+// 						isSelectorClosed = true;
+// 						isSelectorOpened = false;
+// 					}
+// 					continue;
+// 				case SpecialChars.SelectorOrMediaOpen:
+// 					if (rule.type !== CssTypes.media && rule.type !== CssTypes.selector) {
+// 						rule.actualValue += char;
+// 						rule.value += char;
+// 					} else {
+// 						if (rule.subType === CssSubTypes.keyFrames) {
+// 							rule.actualValue += char;
+// 							rule.value += char;
+// 						} else {
+// 							rules.push(rule);
+// 							prevRule = clone(rule);
+// 							rule = {};
+// 							isSelectorOpened = rule.type === CssTypes.selector;
+// 						}
+// 					}
+// 					continue;
+// 				case SpecialChars.SingleComment1:
+// 				case SpecialChars.SingleComment1:
+// 					rule.actualValue += char;
+// 					rule.value += char;
+// 					if (rule.type === CssTypes.comment && rule.subType === CssSubTypes.singleLineComment) {
+// 						rules.push(rule);
+// 						prevRule = clone(rule);
+// 						rule = {};
+// 					}
+// 					continue;
+// 				case SpecialChars.MultiComment:
+// 					rule.actualValue += char;
+// 					rule.value += char;
+// 					if (rule.type === CssTypes.comment && rule.subType === CssSubTypes.multiLineComment) {
+// 						const startLength = rule.actualValue?.split('/*').length;
+// 						const endLength = rule.actualValue?.split('*/').length;
+// 						if (startLength === endLength) {
+// 							rules.push(rule);
+// 							prevRule = clone(rule);
+// 							rule = {};
+// 						}
+// 					}
+// 					continue;
+// 				default:
+// 					break;
+// 			}
+// 		} else {
+// 			if (char === SpecialChars.SelectorOrMediaClose) {
+// 				isSelectorClosed = true;
+// 				continue;
+// 			}
+// 		}
+
+// 		switch (rule.type) {
+// 			case CssTypes.comment:
+// 				if (!rule.value) {
+// 					if (char === '/') {
+// 						rule.subType = CssSubTypes.singleLineComment;
+// 					} else {
+// 						rule.subType = CssSubTypes.multiLineComment;
+// 					}
+// 					rule.value = rule.actualValue;
+// 				}
+// 				rule.value += char;
+// 				rule.actualValue += char;
+// 				break;
+// 			case CssTypes.media:
+// 				if (!rule.value) {
+// 					if (char === '-' || char === 'k') {
+// 						rule.value = rule.actualValue;
+// 						rule.subType = CssSubTypes.keyFrames;
+// 					}
+// 				}
+// 				rule.value += char;
+// 				rule.actualValue += char;
+// 				break;
+// 			case CssTypes.selector:
+// 				rule.value += char;
+// 				rule.actualValue += char;
+// 				break;
+// 			case CssTypes.property:
+// 				rule.value += char;
+// 				rule.actualValue += char;
+// 				break;
+// 			case CssTypes.value:
+// 				rule.value += char;
+// 				rule.actualValue += char;
+// 				break;
+// 			default:
+// 				if (prevRule.type !== CssTypes.property) {
+// 					switch (char) {
+// 						case Chars.Class:
+// 							rule = {
+// 								type: CssTypes.selector,
+// 								subType: CssSubTypes.class,
+// 								value: '',
+// 								actualValue: `${char}`
+// 							};
+// 							path.push(CssTypes.selector);
+// 							break;
+// 						case Chars.Id:
+// 							rule = {
+// 								type: CssTypes.selector,
+// 								subType: CssSubTypes.id,
+// 								value: '',
+// 								actualValue: `${char}`
+// 							};
+// 							path.push(CssTypes.selector);
+// 							break;
+// 						case Chars.Comment:
+// 							rule = {
+// 								type: CssTypes.comment,
+// 								subType: undefined,
+// 								value: ``,
+// 								actualValue: `${char}`
+// 							};
+// 							path.push(CssTypes.comment);
+// 							break;
+// 						case Chars.Media:
+// 							rule = {
+// 								type: CssTypes.media,
+// 								subType: undefined,
+// 								value: '',
+// 								actualValue: `${char}`
+// 							};
+// 							path.push(CssTypes.media);
+// 							break;
+// 						default:
+// 							// '\s,-,[,\\,|,*,:'
+// 							if (char !== ' ' && char != SpecialChars.SingleComment1 && char != SpecialChars.SingleComment2) {
+// 								if (prevRule.type === CssTypes.selector || path[path.length - 1]) {
+// 									rule = {
+// 										type: CssTypes.property,
+// 										subType: undefined,
+// 										value: `${char}`,
+// 										actualValue: `${char}`
+// 									}
+// 								} else {
+// 									rule = {
+// 										type: CssTypes.selector,
+// 										subType: CssSubTypes.element,
+// 										value: `${char}`,
+// 										actualValue: `${char}`
+// 									}
+// 								}
+// 							}
+// 							break;
+// 					}
+// 				} else {
+// 					if (prevRule.type === CssTypes.property && isPropertyClosed) {
+// 						isPropertyClosed = false;
+// 						rule = {
+// 							type: CssTypes.value,
+// 							subType: undefined,
+// 							value: `${char}`,
+// 							actualValue: `${char}`
+// 						}
+// 					}
+// 				}
+// 				break;
+// 		}
+// 	}
+
+// 	console.log(rules);
+
+// 	return [''];// iteratorProps.tokens.map(token => token.value.trim()).filter(token => token);
+// }
 
 // const repeat = (char: string, times: number): string => times ? `${repeat(char, times - 1)}${char}` : ''
 
-const convertoToJS = (tokens: Array<string>) => {
+export const convertoToJS = (tokens: Array<string>) => {
 	const items: Array<Item> = [];
 
 	let nextAction = readSelector;
@@ -483,7 +783,10 @@ const renderItem = (item: Item) => {
 //#region Public Functions
 
 export const CsstoJs = (css: string, convertionProps?: ConvertionProps) => {
-	return `export const resultJson: { [x: string]: { [x: string]: React.CSSProperties } } = ${convertoToJS(tokenizer(css.split('')))}`;
+	convertionAttributes = convertionProps;
+	jsRule = {};
+	convertToJS(css.split(''));
+	return `export const resultJson: { [x: string]: { [x: string]: React.CSSProperties } } = ${JSON.stringify(jsRule)}`;
 }
 
 //#endregion
