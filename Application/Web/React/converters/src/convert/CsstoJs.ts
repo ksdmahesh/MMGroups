ed0df81475c360c94978f0a730e5675d63f133af
Mahesh <ksdmahesh@hotmail.com>
2021-03-04 09:14:50 +0530

    saa

--- Application/Web/React/converters/src/convert/CsstoJs.ts
+++ Application/Web/React/converters/src/convert/CsstoJs.ts
@@ -1,12 +1,3 @@
-//#region Properties
-
-const SPACE = '    ';
-// const whitespc = ['\r\n', '\n\r', '\n', '\r'];
-// const specialChars = ["{", "}", ":", ";", ","];
-// const specialCharsPB = ["{", "}", ";"];
-
-//#endregion
-
 //#region Types
 
 enum Chars {
@@ -51,7 +42,7 @@ type CssRule = {
 	subType?: CssSubTypes,
 	actualValue?: string,
 	value?: string
-}
+};
 
 type JsRule = {
 	// default, media, keyframes
@@ -62,35 +53,7 @@ type JsRule = {
 			[x: string]: string | number
 		}
 	}
-}
-
-type Item = {
-	originalValue: string,
-	selectors: Array<string>,
-	values: { [x: string]: Array<string> }
 };
-
-type ActionProps = {
-	actualItem?: Item,
-	actualProp?: string,
-	items: Array<Item>
-};
-
-// type TokenType = {
-// 	path: Array<string>,
-// 	value: string
-// };
-
-// type IteratorProps = {
-// 	tokens: Array<TokenType>,
-// 	token: TokenType,
-// 	lastChar: string,
-// 	nextChar: string,
-// 	char: string,
-// 	sc?: Array<string>,
-// 	inBrackets: boolean,
-// 	currentPath: Array<string>
-// };
 
 type ConvertionProps = {
 	/**
@@ -125,14 +88,136 @@ type ConvertionProps = {
 	useMaterialThemeStructure?: boolean
 };
 
+enum MediaTypes {
+	all = 'all',
+	print = 'print',
+	screen = 'screen',
+	speech = 'speech',
+	tty = 'tty',
+	tv = 'tv',
+	projection = 'projection',
+	handheld = 'handheld',
+	braille = 'braille',
+	embossed = 'embossed',
+	aural = 'aural'
+}
+
+enum MediaFeatures {
+	/**
+	 * @typedef MediaAnyHover
+	 */
+	anyHover = 'any-hover',
+	/**
+	 * @typedef MediaAnyPointer
+	 */
+	anyPointer = 'any-pointer',
+	/**
+	 * @type Ratio
+	 * @example (aspect-ratio: 1/1)
+	 */
+	aspectRatio = 'aspect-ratio',
+	/**
+	 * @type Ratio
+	 * @example (min-aspect-ratio: 1/1)
+	 */
+	minAspectRatio = 'min-aspect-ratio',
+	/**
+	 * @type Ratio
+	 * @example (max-aspect-ratio: 1/1)
+	 */
+	maxAspectRatio = 'max-aspect-ratio',
+	/**
+	 * @type None
+	 * @example (color)
+	 */
+	color = 'color',
+	/**
+	 * @type Integer
+	 * @example (min-color: 8)
+	 */
+	minColor = 'min-color',
+	/**
+	 * @type Integer
+	 * @example (max-color: 8)
+	 */
+	maxColor = 'max-color',
+	/**
+	 *@typedef MediaColorGamut
+	 */
+	colorGamut = 'color-gamut',
+	/**
+	 * @type None
+	 * @example (color-index)
+	 */
+	colorIndex = 'color-index',
+	/**
+	 * @type Integer
+	 * @example (min-color-index: 8)
+	 */
+	minColorIndex = 'min-color-index',
+	/**
+	 * @type Integer
+	 * @example (max-color-index: 8)
+	 */
+	maxColorIndex = 'max-color-index',
+	deviceAspectRatio = 'device-aspect-ratio',
+	deviceHeight = 'device-height',
+	deviceWidth = 'device-width',
+	displayMode = 'display-mode',
+	forcedColors = 'forced-colors',
+	grid = 'grid',
+	height = 'height',
+	hover = 'hover',
+	invertedColors = 'inverted-colors',
+	monochrome = 'monochrome',
+	orientation = 'orientation',
+	overflowBlock = 'overflow-block',
+	overflowInline = 'overflow-inline',
+	pointer = 'pointer',
+	prefersColorScheme = 'prefers-color-scheme',
+	prefersContrast = 'prefers-contrast',
+	prefersReducedMotion = 'prefers-reduced-motion',
+	prefersReducedTransparency = 'prefers-reduced-transparency',
+	resolution = 'resolution',
+	scan = 'scan',
+	scripting = 'scripting',
+	update = 'update',
+	width = 'width'
+}
+
+enum MediaAnyHover {
+	none,
+	hover
+}
+
+enum MediaAnyPointer {
+	none,
+	coarse,
+	fine
+}
+
+enum MediaColorGamut {
+	srgb,
+	p3,
+	rec2020
+}
+
+enum MediaOperators {
+	not = 'not',
+	and = 'and',
+	only = 'only'
+}
+
 //#endregion
+
+//#region 
 
 const testRule: CssRule[] = [];
 let isSelector = false;
 let isMedia = false;
 let isProperty = false;
 let rule: CssRule = {};
-const path: { type: CssTypes, value: string }[] = [];
+const path: { type: CssTypes, value: string, actualValue: string }[] = [];
 let jsRule: JsRule = {};
 let convertionAttributes: ConvertionProps | undefined;
 const pattern = /-|\.|#|=|\]|\[|\)|\(|:/igm;
@@ -163,24 +248,39 @@ const getPath = (value: string) => {
 const assignRule = () => {
 	let currentRule: any = jsRule;
 	let prevPath: { type: CssTypes, value: string } | undefined = undefined;
-	path.forEach((activePath, index) => {
-		try {
-			if (activePath.type === CssTypes.value) {
-				if (prevPath) {
-					currentRule[prevPath.value] = `${activePath.value || ''}`;
-				}
-			} else {
-				if (!currentRule[activePath.value]) {
-					currentRule[activePath.value] = {};
-				}
-				
-				prevPath = activePath;
-				currentRule = currentRule[activePath.value];
-			}
-		} catch (er) {
+	if ((path?.[0]?.type === CssTypes.comment && (convertionAttributes?.keepComments || convertionAttributes?.keepComments === undefined)) || path?.[0]?.type !== CssTypes.comment) {
+		path?.forEach((activePath, index) => {
+			try {
+				if (!index) {
+					if (activePath.type === CssTypes.media) {
 
-		}
-	});
+					} else {
+						if (!currentRule.default) {
+							currentRule.default = {};
+						}
+
+						prevPath = activePath;
+						currentRule = currentRule.default;
+					}
+				} else {
+					if (activePath.type === CssTypes.value) {
+						if (prevPath) {
+							currentRule[prevPath.value] = `${(convertionAttributes?.includeCssKeys || convertionAttributes?.includeCssKeys === undefined) ? `${activePath.actualValue || ''}\r\n` : ''}${activePath.value || ''}`;
+						}
+					} else {
+						if (!currentRule[activePath.value]) {
+							currentRule[activePath.value] = {};
+						}
+
+						prevPath = activePath;
+						currentRule = currentRule[activePath.value];
+					}
+				}
+			} catch (er) {
+
+			}
+		});
+	}
 
 	testRule.push(rule);
 }
@@ -194,7 +294,7 @@ const convertToJS = (code: string[]) => {
 						rule.actualValue += char;
 						rule.value += char;
 					} else {
-						path.push({ type: rule.type, value: `${rule.actualValue}` });
+						path.push({ type: rule.type, value: `${rule.actualValue}`, actualValue: `${rule.actualValue}` });
 						assignRule();
 						path.pop();
 						path.pop();
@@ -207,7 +307,7 @@ const convertToJS = (code: string[]) => {
 						rule.actualValue += char;
 						rule.value += char;
 					} else {
-						path.push({ type: rule.type, value: getPath(`${rule.actualValue}`) });
+						path.push({ type: rule.type, value: getPath(`${rule.actualValue}`), actualValue: `${rule.actualValue}` });
 						assignRule();
 						isProperty = rule.type === CssTypes.property;
 						rule = {};
@@ -261,7 +361,7 @@ const convertToJS = (code: string[]) => {
 							rule.actualValue += char;
 							rule.value += char;
 						} else {
-							path.push({ type: rule.type, value: getPath(`${rule.actualValue}`) });
+							path.push({ type: rule.type, value: getPath(`${rule.actualValue}`), actualValue: `${rule.actualValue}` });
 							assignRule();
 							isSelector = rule.type === CssTypes.selector;
 							isMedia = rule.type === CssTypes.media;
@@ -408,374 +508,6 @@ const convertToJS = (code: string[]) => {
 	}
 
 	console.log(jsRule)
-}
-
-//#region Private Functions
-
-export const clone = (data: object) => JSON.parse(JSON.stringify(data));
-
-const toProperty = (name: string) => {
-	if (name.charAt(0) === "-") name = name.slice(0);
-
-	return name.replace(/[^a-z0-9]([a-z0-9])?/gi, (_, l) => l ? l.toUpperCase() : '');
-}
-
-const toSelectors = (name: string) => {
-	return name.split(",").map(nameMap);
-}
-
-const nameMap = (currentName: string) => {
-	currentName = currentName.trim();
-	let newName = "";
-
-	if (currentName.charAt(0) === ".") {
-		newName += "Class";
-		currentName = currentName.slice(1);
-	} else if (currentName.charAt(0) === "#") {
-		newName += "Id";
-		currentName = currentName.slice(1);
-	} else {
-		newName += "Element";
-	}
-
-	return `${currentName.replace(/([^a-z0-9])([a-z0-9])?/gi, (_, c, l) => (l ? ((c === "," || c === " ") ? l.toLowerCase() : l.toUpperCase()) : ''))}${newName}`;
-}
-
-// const tokenizer = (code: string[]) => {
-// 	const rule: CssRule = {};
-// 	const jsRule: JsRule = {};
-// 	let path: string[] = [];
-
-// 	for (const char of code) {
-// 		if (rule.type) {
-// 			switch (char) {
-// 				case SpecialChars.Value:
-// 					if (rule.type !== CssTypes.value) {
-// 						rule.actualValue += char;
-// 						rule.value += char;
-// 					} else {
-// 						let currentRule: any = jsRule;
-// 						path.forEach(a => {
-// 							if (!currentRule[a]) {
-// 								currentRule[a] = {};
-// 							}
-// 							currentRule = currentRule[a];
-// 						})
-// 						jsRule[path.join('.')] = `${rule.value || ''}`;
-// 					}
-// 					continue;
-// 				case SpecialChars.Property:
-// 					if (rule.type !== CssTypes.property) {
-// 						rule.actualValue += char;
-// 						rule.value += char;
-// 					} else {
-// 						rules.push(rule);
-// 						prevRule = clone(rule);
-// 						rule = {};
-// 						isPropertyClosed = true;
-// 					}
-// 					continue;
-// 				case SpecialChars.SelectorOrMediaClose:
-// 					if (rule.subType === CssSubTypes.keyFrames) {
-// 						rule.actualValue += char;
-// 						rule.value += char;
-// 						const startLength = rule.actualValue?.split('{').length;
-// 						const endLength = rule.actualValue?.split('}').length;
-// 						if (startLength === endLength) {
-// 							rules.push(rule);
-// 							prevRule = clone(rule);
-// 							rule = {};
-// 						}
-// 					} else {
-// 						rules.push(rule);
-// 						prevRule = clone(rule);
-// 						rule = {};
-// 						isSelectorClosed = true;
-// 						isSelectorOpened = false;
-// 					}
-// 					continue;
-// 				case SpecialChars.SelectorOrMediaOpen:
-// 					if (rule.type !== CssTypes.media && rule.type !== CssTypes.selector) {
-// 						rule.actualValue += char;
-// 						rule.value += char;
-// 					} else {
-// 						if (rule.subType === CssSubTypes.keyFrames) {
-// 							rule.actualValue += char;
-// 							rule.value += char;
-// 						} else {
-// 							rules.push(rule);
-// 							prevRule = clone(rule);
-// 							rule = {};
-// 							isSelectorOpened = rule.type === CssTypes.selector;
-// 						}
-// 					}
-// 					continue;
-// 				case SpecialChars.SingleComment1:
-// 				case SpecialChars.SingleComment1:
-// 					rule.actualValue += char;
-// 					rule.value += char;
-// 					if (rule.type === CssTypes.comment && rule.subType === CssSubTypes.singleLineComment) {
-// 						rules.push(rule);
-// 						prevRule = clone(rule);
-// 						rule = {};
-// 					}
-// 					continue;
-// 				case SpecialChars.MultiComment:
-// 					rule.actualValue += char;
-// 					rule.value += char;
-// 					if (rule.type === CssTypes.comment && rule.subType === CssSubTypes.multiLineComment) {
-// 						const startLength = rule.actualValue?.split('/*').length;
-// 						const endLength = rule.actualValue?.split('*/').length;
-// 						if (startLength === endLength) {
-// 							rules.push(rule);
-// 							prevRule = clone(rule);
-// 							rule = {};
-// 						}
-// 					}
-// 					continue;
-// 				default:
-// 					break;
-// 			}
-// 		} else {
-// 			if (char === SpecialChars.SelectorOrMediaClose) {
-// 				isSelectorClosed = true;
-// 				continue;
-// 			}
-// 		}
-
-// 		switch (rule.type) {
-// 			case CssTypes.comment:
-// 				if (!rule.value) {
-// 					if (char === '/') {
-// 						rule.subType = CssSubTypes.singleLineComment;
-// 					} else {
-// 						rule.subType = CssSubTypes.multiLineComment;
-// 					}
-// 					rule.value = rule.actualValue;
-// 				}
-// 				rule.value += char;
-// 				rule.actualValue += char;
-// 				break;
-// 			case CssTypes.media:
-// 				if (!rule.value) {
-// 					if (char === '-' || char === 'k') {
-// 						rule.value = rule.actualValue;
-// 						rule.subType = CssSubTypes.keyFrames;
-// 					}
-// 				}
-// 				rule.value += char;
-// 				rule.actualValue += char;
-// 				break;
-// 			case CssTypes.selector:
-// 				rule.value += char;
-// 				rule.actualValue += char;
-// 				break;
-// 			case CssTypes.property:
-// 				rule.value += char;
-// 				rule.actualValue += char;
-// 				break;
-// 			case CssTypes.value:
-// 				rule.value += char;
-// 				rule.actualValue += char;
-// 				break;
-// 			default:
-// 				if (prevRule.type !== CssTypes.property) {
-// 					switch (char) {
-// 						case Chars.Class:
-// 							rule = {
-// 								type: CssTypes.selector,
-// 								subType: CssSubTypes.class,
-// 								value: '',
-// 								actualValue: `${char}`
-// 							};
-// 							path.push(CssTypes.selector);
-// 							break;
-// 						case Chars.Id:
-// 							rule = {
-// 								type: CssTypes.selector,
-// 								subType: CssSubTypes.id,
-// 								value: '',
-// 								actualValue: `${char}`
-// 							};
-// 							path.push(CssTypes.selector);
-// 							break;
-// 						case Chars.Comment:
-// 							rule = {
-// 								type: CssTypes.comment,
-// 								subType: undefined,
-// 								value: ``,
-// 								actualValue: `${char}`
-// 							};
-// 							path.push(CssTypes.comment);
-// 							break;
-// 						case Chars.Media:
-// 							rule = {
-// 								type: CssTypes.media,
-// 								subType: undefined,
-// 								value: '',
-// 								actualValue: `${char}`
-// 							};
-// 							path.push(CssTypes.media);
-// 							break;
-// 						default:
-// 							// '\s,-,[,\\,|,*,:'
-// 							if (char !== ' ' && char != SpecialChars.SingleComment1 && char != SpecialChars.SingleComment2) {
-// 								if (prevRule.type === CssTypes.selector || path[path.length - 1]) {
-// 									rule = {
-// 										type: CssTypes.property,
-// 										subType: undefined,
-// 										value: `${char}`,
-// 										actualValue: `${char}`
-// 									}
-// 								} else {
-// 									rule = {
-// 										type: CssTypes.selector,
-// 										subType: CssSubTypes.element,
-// 										value: `${char}`,
-// 										actualValue: `${char}`
-// 									}
-// 								}
-// 							}
-// 							break;
-// 					}
-// 				} else {
-// 					if (prevRule.type === CssTypes.property && isPropertyClosed) {
-// 						isPropertyClosed = false;
-// 						rule = {
-// 							type: CssTypes.value,
-// 							subType: undefined,
-// 							value: `${char}`,
-// 							actualValue: `${char}`
-// 						}
-// 					}
-// 				}
-// 				break;
-// 		}
-// 	}
-
-// 	console.log(rules);
-
-// 	return [''];// iteratorProps.tokens.map(token => token.value.trim()).filter(token => token);
-// }
-
-// const repeat = (char: string, times: number): string => times ? `${repeat(char, times - 1)}${char}` : ''
-
-export const convertoToJS = (tokens: Array<string>) => {
-	const items: Array<Item> = [];
-
-	let nextAction = readSelector;
-	tokens.forEach(token => {
-		nextAction = nextAction(token, { actualItem: undefined, actualProp: undefined, items });
-	});
-
-	return renderJS(items);
-}
-
-const readSelector = (token: string, props: ActionProps) => {
-	const selectors = toSelectors(token);
-
-	props.actualItem = {
-		originalValue: token,
-		selectors,
-		values: {}
-	};
-
-	props.actualProp = undefined;
-	props.items.push(props.actualItem);
-
-	return readBracketO;
-}
-
-const readBracketO = (token: string, _: ActionProps) => {
-	if (token !== "{") {
-		throw new Error("Era esperado um '{' ");
-	}
-
-	return readProperty;
-}
-
-const readProperty = (token: string, props: ActionProps) => {
-	if (token === "}") {
-		return readBracketC(token, props);
-	}
-
-	const property = toProperty(token);
-	props.actualProp = property;
-
-	if (props.actualItem && !props.actualItem.values?.[property]) {
-		props.actualItem.values[property] = [];
-	}
-
-	return readDefinition;
-}
-
-const readDefinition = (token: string, _: ActionProps) => {
-	if (token !== ":") {
-		throw new Error("Era esperado um ':' ");
-	}
-
-	return readValue;
-}
-
-const readValue = (token: string, props: ActionProps) => {
-	if (props.actualItem) {
-		props.actualItem.values[props.actualProp || ''].push(token);
-	}
-
-	return readFinal;
-}
-
-const readFinal = (token: string, props: ActionProps) => {
-	if (token === "}") {
-		return readBracketC(token, props);
-	}
-
-	if (token !== ";") {
-		throw new Error("Era esperado um ';' ");
-	}
-
-	return readProperty;
-}
-
-const readBracketC = (token: string, _: ActionProps) => {
-	if (token !== "}") {
-		throw new Error("Era esperado um '}' ");
-	}
-
-	return readSelector;
-}
-
-const renderJS = (items: Array<Item>) => `const styles = {\n${items.map(renderItem).join(",")}\n}`
-
-const renderItem = (item: Item) => {
-	const code = ["\n//" + item.originalValue];
-
-	let properties: Array<string> = [];
-
-	for (const prop in item.values) {
-		const propitem = {
-			name: prop,
-			value: item.values[prop][item.values[prop].length - 1]
-		};
-
-		let markup = '"';
-		if (~propitem.value.indexOf('"')) {
-			markup = "'";
-			propitem.value = propitem.value.replace(/'/gi, "\\'");
-		}
-
-		properties.push(SPACE + SPACE + propitem.name + ": " + markup + propitem.value + markup);
-	}
-
-	properties = properties.map(x => `${SPACE}${x}`);
-
-	item.selectors.forEach(i => {
-		code.push(`${SPACE}${i}: {`);
-		code.push(properties.join(",\n"));
-		code.push(`${SPACE}}`);
-	});
-
-	return code.join("\n");
 }
 
 //#endregion
